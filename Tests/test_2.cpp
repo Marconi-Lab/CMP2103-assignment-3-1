14df3c540ed149829a2e4391e052f1733a8d1c8e
PaulOkwija <okwijapaul@gmail.com>
2022-11-16 22:45:47 +0300

    Updated

--- Tests/test_2.cpp
+++ Tests/test_2.cpp
@@ -2,19 +2,7 @@
 #include "catch.hpp"
 #include "../main.h"
 
-std::string Eq_triangle = "The triangle is an equilateral triangle";
-std::string Iso_triangle = "The triangle is an isosceles triangle";
-std::string Right_ang_triangle = "The triangle is a right-angled triangle";
-std::string Sca_triangle = "The triangle is a scalene triangle";
-
-TEST_CASE( "Tests_3", "Tests" ) {
-    REQUIRE(check_triangle(10,8,6) == Right_ang_triangle);
-    REQUIRE(check_triangle(13,13,25) == Iso_triangle);
-    REQUIRE(check_triangle(3,31,30) == Sca_triangle);
-    REQUIRE(check_triangle(15,9,12) == Right_ang_triangle);
-    REQUIRE(check_triangle(2,4,3) == Sca_triangle);
-}
-TEST_CASE( "Tests_4", "Tests" ) {
-    REQUIRE( check_triangle(8,10,6) == Right_ang_triangle);
+TEST_CASE( "Tests_2", "Tests" ) {
+    REQUIRE( (area(radius(3.141593)) - 1.5708) < 1e-3);
 }
 
--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main(){
 std::cout<<"Areas given 10 is: "<<area(radius(10))<<std::endl;   //Answer is 15.9155
 std::cout<<"Areas given 0.6 is: "<<area(radius(0.6))<<std::endl; //Answer is 0.0572958
 std::cout<<"Areas given 19.6 is: "<<area(radius(19.6))<<std::endl; //Answer is 61.141
+
 }
 
